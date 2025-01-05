ce6b467f1365e11335261f7d1c4e7ef6fe10b130
Andres Sanchez <and_sg@outlook.com>
2025-01-04 21:17:48 -0500

    add tests for add and query a movie at index 0

--- FilmFestTests/Test Controllers/MovieManagerTests.swift
+++ FilmFestTests/Test Controllers/MovieManagerTests.swift
@@ -19,6 +19,8 @@ class MovieManagerTests: XCTestCase {
     override func tearDown(){
         super.tearDown()
     }
+    
+    //MARK: Initial values
     // keep the track of the movies we want to see
     // keep the track of the movies we have already seen
     // be sure that the variables are set to zero by default
@@ -30,6 +32,24 @@ class MovieManagerTests: XCTestCase {
     func testInit_MoviesSeen_ReturnZero(){
         XCTAssertEqual(sut.moviesSeenCount, 0)
         
+    }
+    //MARK: Add and query
+    //add movies to library
+    //increase the count each time
+    
+    func testAdd_MoviesToSee_ReturnsOne(){
+        let testMovie = Movie(title: "Sci-Fi")
+        sut.addMovie(movie: testMovie)
+        // one after add the movie to the library
+        XCTAssertEqual(sut.moviesToSeeCount, 1)
+    }
+    
+    func testQuery_ReturnMovieAtIndex(){
+        let testMovie = Movie(title: "Arthouse Drama")
+        sut.addMovie(movie: testMovie)
+        
+        let movieQueried = sut.movieAtIndex(index: 0 )
+        XCTAssertEqual(testMovie.title, movieQueried.title)
     }
     
 }
