1a97aa1ef59c90156abb40ccf58bdb49883810f1
Andres Sanchez <and_sg@outlook.com>
2025-01-04 21:00:15 -0500

    refactor sut to be shared among tests

---
+++ FilmFest/Controllers/MovieManager.swift
@@ -0,0 +1,15 @@
+//
+//  MovieManager.swift
+//  FilmFest
+//
+//  Created by Andres Sanchez on 04/01/2025.
+//  Copyright © 2025 Author. All rights reserved.
+//
+
+import Foundation
+
+class MovieManager{
+    let moviesToSeeCount = 0
+    let moviesSeenCount = 0
+    
+}
--- FilmFestTests/Test Controllers/MovieManagerTests.swift
+++ FilmFestTests/Test Controllers/MovieManagerTests.swift
@@ -11,12 +11,25 @@ import XCTest
 @testable import FilmFest
 
 class MovieManagerTests: XCTestCase {
+    var sut: MovieManager!
     override func setUp(){
         super.setUp()
+        sut = MovieManager()
     }
     override func tearDown(){
         super.tearDown()
     }
-    
+    // keep the track of the movies we want to see
+    // keep the track of the movies we have already seen
+    // be sure that the variables are set to zero by default
 
+    func testInit_MoviesToSee_ReturnZero(){
+        XCTAssertEqual(sut.moviesToSeeCount, 0)
+    }
+    
+    func testInit_MoviesSeen_ReturnZero(){
+        XCTAssertEqual(sut.moviesSeenCount, 0)
+        
+    }
+    
 }
